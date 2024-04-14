de556a9e5ece7df2f31f4d3f3435981e98df81bf
Anurag Sidhu <anuragsidhu72@gmail.com>
2024-04-14 16:52:24 +0530

    Loading done

--- src/app/custom/Hooks/useProblemData.ts
+++ src/app/custom/Hooks/useProblemData.ts
@@ -6,6 +6,7 @@ import { db } from '../../../../firebaseConfig';
 
 const useProblemData = () => {
     const [data, setData] = useState<any[]>([]);
+    const [loading, setLoading] = useState(true)
 
     const getDataCallback = useCallback(async () => {
         try {
@@ -15,12 +16,13 @@ const useProblemData = () => {
                 store.push(doc.data());
             });
             setData(store);
+            setLoading(false)
         } catch (error) {
             console.log(error);
         }
     }, []);
 
-    return { data, getDataCallback };
+    return { data, getDataCallback, loading };
 };
 
 export default useProblemData;
--- src/app/custom/compnentdata/Table.tsx
+++ src/app/custom/compnentdata/Table.tsx
@@ -23,43 +23,46 @@ type Problem = {
 
 const Table = () => {
 
-    const { data, getDataCallback } = useProblemData();
-
-    console.log(data);
+    const { data, getDataCallback, loading } = useProblemData();
 
 
     useEffect(() => {
         getDataCallback();
     }, []);
 
-
     return (
         <div className='bg-black h-screen space-y-48  flex items-center justify-center flex-col  '>
-            <table>
-                <TableCaption>A list of All Problem</TableCaption>
-                <TableHeader>
-                    <TableRow>
-                        <TableHead className="w-[400px]">Title</TableHead>
-                        <TableHead className="w-[400px]">Difficulty</TableHead>
-                        <TableHead>Method</TableHead>
-                    </TableRow>
-                </TableHeader>
-                <TableBody className='text-white'>
-                    {data.map((problem, index) => (
-                        <TableRow key={index}>
-                            <TableCell className="font-medium">
-                                {problem.Title}
-                            </TableCell>
-                            <TableCell>{problem.Difficulty}</TableCell>
-                            <TableCell>
-                                <a href={`/Details/${problem.id}`}>
-                                    Solve
-                                </a>
-                            </TableCell>
-                        </TableRow>
-                    ))}
-                </TableBody>
-            </table>
+            {
+
+                loading ? (<p className='text-white'>Loading...</p>) : (<>
+                    <table>
+                        <TableCaption>A list of All Problem</TableCaption>
+                        <TableHeader>
+                            <TableRow>
+                                <TableHead className="w-[400px]">Title</TableHead>
+                                <TableHead className="w-[400px]">Difficulty</TableHead>
+                                <TableHead>Method</TableHead>
+                            </TableRow>
+                        </TableHeader>
+                        <TableBody className='text-white'>
+                            {data.map((problem, index) => (
+                                <TableRow key={index}>
+                                    <TableCell className="font-medium">
+                                        {problem.Title}
+                                    </TableCell>
+                                    <TableCell>{problem.Difficulty}</TableCell>
+                                    <TableCell>
+                                        <a href={`/Details/${problem.id}`}>
+                                            Solve
+                                        </a>
+                                    </TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </table>
+                </>)
+            }
+
             <Footer />
         </div>
     );
